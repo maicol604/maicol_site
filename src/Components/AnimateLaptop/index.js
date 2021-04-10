7766281dcd5574ad652c63ea7bb9edba6516a34a
maicol604 <maicol.fario@gmail.com>
2021-04-09 22:59:41 -0500

    test f

--- src/Components/AnimateLaptop/index.js
+++ src/Components/AnimateLaptop/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles.scss';
 
+import laptop2 from '../../img/mac-keyboard-min.png';
+
 const Laptop = (props) => {
     return (
         <div className='laptop-wrapper'>
             <img className={'animate-screen '+(props.active?'animate-screen-active':'')} src={require('../../img/mac-min.png')} alt='laptop' />
             <span className={'coding '+(props.active?'coding-active':'')}><img src={require('../../img/coding.gif')} alt='coding' /></span>
-            <img className='keyboard' src={require('../../img/mac-keyboard-min.png')} alt='laptop' />
+            <img className='keyboard' src={laptop2} alt='laptop' />
         </div>
     )
 }
--- src/Components/MainImgUser/index.js
+++ src/Components/MainImgUser/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './styles.scss';
 
+import image from '../../img/maicol-main-min.jpg';
+
 const MainImgUser = (props) => {
     return (
         <div className='main-img-user'>
-            <img src={require('../../img/maicol-main-min.jpg')} alt='user' />
+            <img src={image} alt='user' />
             <div className='name'>
                 <h2>MAICOL</h2>
             </div>
